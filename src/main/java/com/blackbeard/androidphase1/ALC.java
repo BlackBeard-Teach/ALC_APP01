a34f701182a58a8b78f9408688906e5c33da83ef
Keamo <39981840+BlackBeard-Teach@users.noreply.github.com>
2019-07-15 01:24:53 +0200

    first commit

---
+++ src/main/java/com/blackbeard/androidphase1/ALC.java
@@ -0,0 +1,33 @@
+package com.blackbeard.androidphase1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class ALC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alc);
+
+        WebView webView = findViewById(R.id.alc_web);
+        webView.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
+        webView.loadUrl("https://andela.com/about/");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+    }
+}
